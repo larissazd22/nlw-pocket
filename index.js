9c103dde5ba0dcc7a78775a14f8c6ef35d57c411
larissazd22 <larissazacheu@hotmail.com>
2024-09-13 18:16:24 -0300

    adiciona inquirer

---
+++ index.js
@@ -0,0 +1,34 @@
+const { select } = require('@inquirer/prompts')
+const start = async () => {
+
+  while(true){
+
+    const opcao = await select({
+      message: "Menu >",
+      choices: [
+        {
+          name: "Cadastrar meta",
+          value: "cadastar"
+        },
+        {
+          name:"Sair",
+          value: "sair"
+        }
+      ]
+    })
+
+
+    switch(opcao) {
+      case "cadastrar":
+      console.log("Vamos cadastrar")
+      break
+      case "listar":
+        console.log("Vamos listar")
+        break
+        case "sair":
+          return
+    }
+  }
+}
+
+start();
